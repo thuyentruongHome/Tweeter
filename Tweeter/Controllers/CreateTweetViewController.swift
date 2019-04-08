aac08cb3ed72c98e11e99aabc73a88fe743b94d5
Thuyen Truong <quynhthuyentruong@gmail.com>
2019-04-08 19:53:29 +0700

    [f] Create Tweet: create CreateTweetVC and its connection

---
+++ Tweeter/Controllers/CreateTweetViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateTweetViewController.swift
+//  Tweeter
+//
+//  Created by Macintosh on 4/8/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class CreateTweetViewController: UIViewController {
+  
+}
